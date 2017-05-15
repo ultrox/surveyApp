558e51126fd1beab4bcf5c10bc44ca793ba694e1
Marko Vujanic <marko.vujan@gmail.com>
2017-05-15 14:14:26 +0200

    Add first stage forms

--- src/components/SurveyForm.js
+++ src/components/SurveyForm.js
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
+import states from '../data/states';
+import TextField from '../common/TextField'
+
+
+import map from 'lodash/map';
 
 class SurveyForm extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			firstname: '',
+			lastname: '',
+			email:'',
+			annualincome: '',
+			state: ''
+		}
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log(this.state);
+	}
 	render() {
+		const options = map(states, (val, key) => {
+			return <option key={val} value={val}>{key}</option>
+		})
+
 		return(
-			<form>
-				<h2>Survey</h2>
-				<label className="control-label"/>
-				<input />
+			<form onSubmit={this.handleSubmit}>
+				<h2>Please Fill the Survey</h2>
+
+				<TextField 
+					fieldName='firstname' 
+					label='First Name' 
+					firstname={this.state.firstname} 
+					handleChange={this.handleChange}
+				/>
+
+				<TextField 
+					fieldName='lastname' 
+					label='Last Name' 
+					firstname={this.state.lastname} 
+					handleChange={this.handleChange}
+				/>
+
+				<TextField 
+					fieldName='email' 
+					label='Email' 
+					firstname={this.state.email} 
+					handleChange={this.handleChange}
+				/>
+
+				<TextField 
+					fieldName='annualincome' 
+					label='Annual Income' 
+					firstname={this.state.annualincome} 
+					handleChange={this.handleChange}
+				/>
+
+				<div className="form-group">
+					<label className="control-label">State</label>
+					<select className='form-control' name='state' value={this.state.state} onChange={this.handleChange}>
+						<option value="" disabled > Choose Your State</option>
+						{options}
+					</select>
+				</div>
+				<div className='form-group'>
+					<button className="btn btn-primary bnt-lg">
+						Send
+					</button>
+				</div>
 			</form>
 		)
 	}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
-import SurveyForm from './components/SurveyForm.js';
+import SurveyPage from './components/SurveyPage.js';
 import About from './components/About.js';
 
 import App from './components/App';
 
 export default (
 	<Route path='/' component={App}>
-		<IndexRoute component={SurveyForm} />
+		<IndexRoute component={SurveyPage} />
 		<Route path="/about" component={About} />
 	</Route>
 )
